dd7deba992b6f07719ad778ef01858676f645c9c
ulzcq <syj9913@ynu.ac.kr>
2022-09-19 23:58:31 +0900

    [기본 문법] JPQL과 비교 및 기본 Q-Type 활용
    JPQL: 문자(실행 시점 오류), Querydsl: 코드(컴파일 시점 오류)
    JPQL: 파라미터 바인딩 직접, Querydsl: 파라미터 바인딩 자동 처리  => Querydsl은 JPQL 빌더
    
    JPAQueryFactory를 필드로 제공하면 동시성 문제는 어떻게 될까?
    동시성 문제는 JPAQueryFactory를 생성할 때 제공하는 EntityManager(em)에 달려있다.
    스프링 프레임워크는 여러 쓰레드에서 동시에 같은 EntityManager에 접근해도,
    트랜잭션 마다 별도의 영속성 컨텍스트를 제공하기 때문에, 동시성 문제는 걱정하지 않아도 된다.

---
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -0,0 +1,82 @@
+package study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
+import study.querydsl.entity.Team;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static study.querydsl.entity.QMember.member;
+
+@SpringBootTest
+@Transactional
+public class QuerydslBasicTest {
+
+    @PersistenceContext
+    EntityManager em;
+
+    JPAQueryFactory queryFactory; //동시성 문제 없게 설계되어있음!
+
+    @BeforeEach
+    public void before() {
+        queryFactory = new JPAQueryFactory(em);
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
+
+    @Test
+    public void startJPQL() {
+        //member1을 찾아라.
+        String qlString =
+                "select m from Member m " +
+                "where m.username = :username"; //오타 발생 시 Runtime 오류 발생
+
+        Member findMember = em.createQuery(qlString, Member.class)
+                .setParameter("username", "member1")
+                .getSingleResult();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    /**
+     * 기본 Q-Type 활용
+     * - 같은 테이블을 조인해야 하는 경우가 아니면 기본 인스턴스를 사용하자
+     * - Querydsl은 컴파일 시점에 오류를 잡아줌
+     */
+    @Test
+    public void startQuerydsl() {
+        //member1을 찾아라.
+
+        //1. 별칭 직접 지정
+//        QMember m = new QMember("m");
+
+        //*권장하는 방법
+        //2. 기본 인스턴스를 static import와 함께 사용 (QMember.member)
+        Member findMember = queryFactory
+                .select(member)
+                .from(member)
+                .where(member.username.eq("member1"))//파라미터 바인딩 자동 처리
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+}
