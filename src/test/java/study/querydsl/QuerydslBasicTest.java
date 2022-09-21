8bff2a0be99c09370234bce284e13ed44dfa3e08
ulzcq <syj9913@ynu.ac.kr>
2022-09-22 00:11:25 +0900

    [중급 문법] 동적 쿼리 - BooleanBuilder 사용

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -752,4 +752,31 @@ public class QuerydslBasicTest {
         }
     }
 
+    /** 동적 쿼리 - 1. BooleanBuilder 사용 */
+    @Test
+    public void 동적쿼리_BooleanBuilder() throws Exception {
+        String usernameParam = "member1";
+        Integer ageParam = null;
+
+        List<Member> result = searchMember1(usernameParam, ageParam);
+        assertThat(result.size()).isEqualTo(1);
+    }
+
+    private List<Member> searchMember1(String usernameCond, Integer ageCond) {
+
+        //BooleanBuilder builder = new BooleanBuilder(member.username.equ(usernameCond); //초기 필수값 설정 예시
+        BooleanBuilder builder = new BooleanBuilder();
+        if (usernameCond != null) {
+            builder.and(member.username.eq(usernameCond));
+        }
+
+        if (ageCond != null) {
+            builder.and(member.age.eq(ageCond));
+        }
+
+        return queryFactory
+                .selectFrom(member)
+                .where(builder)
+                .fetch();
+    }
 }
