148262f024d6d03a01737ee2c448019259520d5c
ulzcq <syj9913@ynu.ac.kr>
2022-09-22 00:56:16 +0900

    [중급 문법] SQL function 호출하기

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -869,4 +869,34 @@ public class QuerydslBasicTest {
         em.clear();
     }
 
+    /**
+     * SQL function 호출하기
+     * - SQL function은 JPA와 같이 Dialect에 등록된 내용만 호출할 수 있다.
+     */
+    @Test
+    public void sqlFunction() {
+        //replace 함수 사용
+        String result = queryFactory
+                .select(Expressions.stringTemplate("function('replace', {0}, {1}, {2})",
+                        member.username, "member", "M")) //이름에서 member라는 단어를 M으로 변경
+                .from(member)
+                .fetchFirst();
+
+        System.out.println("result = " + result);
+    }
+
+    /** lower 같은 ansi 표준 함수들은 querydsl이 상당부분 내장하고 있다. 따라서 다음과 같이 처리해도 결과는 같다. */
+    @Test
+    public void sqlFunction2() {
+        //소문자로 변경해서 비교해라.
+        String result = queryFactory
+                .select(member.username)
+                .from(member)
+//                .where(member.username.eq(Expressions.stringTemplate("function('lower', {0})", member.username)))
+                .where(member.username.eq(member.username.lower())) //querydsl 함수 사용
+                .fetchFirst();
+
+        System.out.println("result = " + result);
+    }
+
 }
