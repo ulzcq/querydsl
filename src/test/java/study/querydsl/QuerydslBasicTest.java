eee9d50774edae0d624489f81b74115ab8a20393
ulzcq <syj9913@ynu.ac.kr>
2022-09-22 00:09:57 +0900

    [중급 문법] 프로젝션 결과 반환 - @QueryProjection

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -1,5 +1,6 @@
 package study.querydsl;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.group.GroupBy;
@@ -10,11 +11,13 @@ import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.core.types.dsl.NumberExpression;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberDto;
+import study.querydsl.dto.QMemberDto;
 import study.querydsl.dto.UserDto;
 import study.querydsl.entity.Member;
 import study.querydsl.entity.QMember;
@@ -663,6 +666,7 @@ public class QuerydslBasicTest {
      *
      * - Querydsl 빈 생성(Bean population)
      * - 결과를 DTO 반환할 때 사용, 다음 3가지 방법 지원
+     * - 단점: Runtime error 발생 가능성
      * 1. 프로퍼티 접근
      */
     @Test
@@ -723,6 +727,23 @@ public class QuerydslBasicTest {
                 .select(Projections.constructor(MemberDto.class, //필드 타입이 딱 맞아야함
                         member.username,
                         member.age))
+                .from(member)
+                .fetch();
+
+        for (MemberDto memberDto : result) {
+            System.out.println("memberDto = " + memberDto);
+        }
+    }
+
+    /** 프로젝션과 결과 반환 - @QueryProjection
+     *
+     * - 이 방법은 컴파일러로 타입을 체크할 수 있으므로 가장 안전한 방법이다.
+     * 다만 DTO에 QueryDSL 어노테이션을 유지해야 하는 점(의존)과 DTO까지 Q파일을 생성해야 하는 단점이 있다.
+     */
+    @Test
+    public void findDtoByQueryProjection(){
+        List<MemberDto> result = queryFactory
+                .select(new QMemberDto(member.username, member.age))
                 .from(member)
                 .fetch();
 
