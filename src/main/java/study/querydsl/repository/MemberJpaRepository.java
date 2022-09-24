e0d91d7c7c3d474647a0674712760aad4b63a8c9
ulzcq <syj9913@ynu.ac.kr>
2022-09-24 17:14:10 +0900

    :pushpin: 3. 동적 쿼리와 성능 최적화 조회 - Where절 파라미터 적용
    null만 조심하면 여러 조건을 조립하는게 가능하고, 재사용성도 좋아서 실무에서 사용하기 좋음

--- src/main/java/study/querydsl/repository/MemberJpaRepository.java
+++ src/main/java/study/querydsl/repository/MemberJpaRepository.java
@@ -1,8 +1,11 @@
 package study.querydsl.repository;
 
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.ObjectUtils;
+import org.springframework.util.StringUtils;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.dto.QMemberTeamDto;
@@ -13,6 +16,7 @@ import javax.persistence.EntityManager;
 import java.util.List;
 import java.util.Optional;
 
+import static org.springframework.util.ObjectUtils.isEmpty;
 import static org.springframework.util.StringUtils.hasText;
 import static study.querydsl.entity.QMember.*;
 import static study.querydsl.entity.QTeam.team;
@@ -92,4 +96,60 @@ public class MemberJpaRepository {
                 .where(builder)
                 .fetch();
     }
+
+    /**
+     * Where절 파라미터 적용, 실무에서 선호하신다고 함
+     * - 회원명, 팀명, 나이(ageGoe, ageLoe)
+     */
+    public List<MemberTeamDto> search(MemberSearchCondition condition) {
+        return queryFactory
+                .select(new QMemberTeamDto(
+                        member.id,
+                        member.username,
+                        member.age,
+                        team.id,
+                        team.name))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe()))
+                .fetch();
+    }
+
+    //조건 거를 때 긍정(hasNext) 부정(isEmpty) 아무거나 선택
+    private BooleanExpression usernameEq(String username) {
+        return isEmpty(username) ? null : member.username.eq(username);
+    }
+
+    private BooleanExpression teamNameEq(String teamName) {
+        return isEmpty(teamName) ? null : team.name.eq(teamName);
+    }
+
+    private BooleanExpression ageGoe(Integer ageGoe) {
+        return ageGoe == null ? null : member.age.goe(ageGoe);
+    }
+
+    private BooleanExpression ageLoe(Integer ageLoe) {
+        return ageLoe == null ? null : member.age.loe(ageLoe);
+    }
+
+    // null만 조심하면 조립 가능(몇가지 필수 조건이 맞아야 데이터가 나가는 경우 사용하면 굳!)
+    // age 조건 2개 조립 메서드 예시 (전체 조건 4개 조립도 가능함)
+    private BooleanExpression ageBetween(int ageLoe, int ageGoe){
+        return ageLoe(ageLoe).and(ageGoe(ageGoe));
+    }
+
+    // select projections 이 달라져도 (MemberTeamDto -> Member) 조건 재사용 가능
+    public List<Member> findMember(MemberSearchCondition condition) {
+        return queryFactory
+                .selectFrom(member)
+                .leftJoin(member.team, team)
+                .where(usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageBetween(condition.getAgeLoe(), condition.getAgeGoe())) //조립 메서드 사용
+                .fetch();
+    }
+
 }
