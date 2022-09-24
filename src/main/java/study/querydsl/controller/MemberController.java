5f472ee0e713980a2cfcf850a888ff61d16c7e26
ulzcq <syj9913@ynu.ac.kr>
2022-09-24 23:00:44 +0900

    :pushpin: 스프링 데이터 페이징 활용 - Querydsl 페이징
    CountQuery 최적화, 컨트롤러 개발까지 해본다.
    스프링 데이터의 정렬을 Querydsl의 정렬로 직접 전환하는 방법은 깃 이슈를 참고
    
    Issues: #4

--- src/main/java/study/querydsl/controller/MemberController.java
+++ src/main/java/study/querydsl/controller/MemberController.java
@@ -1,11 +1,14 @@
 package study.querydsl.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.repository.MemberJpaRepository;
+import study.querydsl.repository.MemberRepository;
 
 import java.util.List;
 
@@ -15,9 +18,16 @@ import java.util.List;
 public class MemberController {
 
     private final MemberJpaRepository memberJpaRepository;
+    private final MemberRepository memberRepository;
 
     @GetMapping("/v1/members")
     public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
         return memberJpaRepository.search(condition);
+    }
+
+    // 포스트맨으로 조회: http://localhost:8080/v2/members?size=5&page=2
+    @GetMapping("/v2/members")
+    public Page<MemberTeamDto> searchMemberV2(MemberSearchCondition condition, Pageable pageable) {
+        return memberRepository.searchPage(condition, pageable);
     }
 }
--- src/main/java/study/querydsl/repository/MemberRepositoryCustomImpl.java
+++ src/main/java/study/querydsl/repository/MemberRepositoryCustomImpl.java
@@ -1,10 +1,16 @@
 package study.querydsl.repository;
 
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.dto.QMemberTeamDto;
+import study.querydsl.entity.Member;
 import study.querydsl.entity.QMember;
 import study.querydsl.entity.QTeam;
 
@@ -56,4 +62,51 @@ public class MemberRepositoryCustomImpl implements MemberRepositoryCustom {
     private BooleanExpression ageLoe(Integer ageLoe) {
         return ageLoe == null ? null : member.age.loe(ageLoe);
     }
+
+    /**
+     * 페이징
+     * - 데이터 조회 쿼리와, 전체 카운트 쿼리(최적화: order by 쓰지 X)를 분리
+     * - 코드를 리펙토링해서, 내용 쿼리과 전체 카운트 쿼리를 읽기 좋게 분리하면 좋다. -> 메소드로 뽑기
+     * @return Page
+     */
+    @Override
+    public Page<MemberTeamDto> searchPage(MemberSearchCondition condition, Pageable pageable) {
+        List<MemberTeamDto> content = queryFactory
+                .select(new QMemberTeamDto(
+                        member.id,
+                        member.username,
+                        member.age,
+                        team.id,
+                        team.name))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe()))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        JPAQuery<Member> countQuery = queryFactory
+                .select(member)
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe()));
+
+        /**
+         *  Count 쿼리 최적화
+         * - 스프링 데이터 라이브러리가 제공
+         * - count 쿼리가 생략 가능한 경우 생략해서 처리
+         *   - 페이지 시작이면서 컨텐츠 사이즈가 페이지 사이즈보다 작을 때
+         *   - 마지막 페이지 일 때 (offset + 컨텐츠 사이즈를 더해서 전체 사이즈 구함)
+         *   - 위 두경우 페이지 나눌 필요가 없으니까 total값이 필요X!
+         */
+//        return new PageImpl<>(content, pageable, total);
+        return PageableExecutionUtils.getPage(content, pageable, () -> countQuery.fetch().size());
+    }
+
 }
